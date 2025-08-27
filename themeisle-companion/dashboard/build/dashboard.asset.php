f1be4ba0f492cc147fa99b8391b42e085be4a5f3
Justin Gardner <justin@rhynorater.com>
2025-08-27 06:05:02 -0500

    3.0.1

--- themeisle-companion/dashboard/build/dashboard.asset.php
+++ themeisle-companion/dashboard/build/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '408703dfb3200fde2b47582f202fd193');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '54d40edd6453facfd740');
